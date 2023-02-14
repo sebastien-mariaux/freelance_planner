a05399f9b3df9055b21e77885a8738c0b7df0eb5
Sébastien Mariaux <sebastien.mariaux@protonmail.com>
2023-02-14 22:39:17 +0100

    Add expenses and totals

--- src/App.js
+++ src/App.js
@@ -8,24 +8,47 @@ const WEEKS_IN_YEAR = 52;
 
 const defaultSimulations = [
   (new Simulation()).serialize(),
+  (new Simulation()).serialize(),
+  (new Simulation()).serialize(),
+  (new Simulation()).serialize(),
   (new Simulation()).serialize()
 ]
 
 
 const fields = [
- {label: 'name', title: 'Titre', style: {}},
- {label: 'dailyRate', title: 'TJM', style: {}},
- {label: 'daysPerWeek', title: 'Jours par semaine', style: {}},
- {label: 'weeksOff', title: 'Semaines off', style: {}},
- {label: 'weeksOn', title: 'Semaines travaillées', style: {}},
- {label: 'yearlyIncome', title: 'CA annuel', style: {fontWeight: 'bold', fontSize: '1.2em'}},
- {label: 'monthlyIncome', title: 'CA moyen mensuel', style: {fontWeight:'bold', borderBottom: '2px solid black', paddingBottom: '15px'  }},
+  { label: 'name', title: 'Titre', style: {} },
+  { label: 'dailyRate', title: 'TJM', style: {} },
+  { label: 'daysPerWeek', title: 'Jours par semaine', style: {} },
+  { label: 'weeksOff', title: 'Semaines off', style: {} },
+  { label: 'weeksOn', title: 'Semaines travaillées', style: {} },
+  { label: 'yearlyIncome', title: 'CA annuel', style: { fontWeight: 'bold', fontSize: '1.2em' } },
+  { label: 'monthlyIncome', title: 'CA moyen mensuel', style: { fontWeight: 'bold', borderBottom: '2px solid black', paddingBottom: '15px' } },
 ];
 
 const expenses = [
- {label: 'charges', title: 'Charges', style: {fontWeight: 'bold', fontSize: '1.2em'}},
+  { label: 'dailyFoodCost', title: 'Prix moyen repas', style: {} },
+  { label: 'yearlyFoodCost', title: 'Coût annuel repas', style: {} },
+  { label: 'yearlyRent', title: 'Coût annuel loyer', style: {} },
+  { label: 'yearlyAccountingCost', title: 'Coût annuel comptabilité', style: {} },
+  { label: 'yearlyInternetCost', title: 'Coût annuel internet', style: {} },
+  { label: 'yearlyPhoneCost', title: 'Coût annuel téléphone', style: {} },
+  { label: 'yearlyProInsuranceCost', title: 'Coût annuel RC Pro', style: {} },
+  { label: 'yearlyOtherInsuranceCost', title: 'Coût annuel autres assurances', style: {} },
+  { label: 'yearlyBankingCost', title: 'Frais bancaires annuels', style: {} },
+  { label: 'yearlyFurnitureCost', title: 'Coût annuel fournitures', style: {} },
+  { label: 'yearlyOtherCost', title: 'Autres coûts annuels', style: {} },
+  { label: 'yearlyTotalCost', title: 'Total des charges', style: { fontWeight: 'bold', fontSize: '1.2em' } },
+  { label: 'montlyAverageCost', title: 'Charges moyennes mensuelles', style: { fontWeight: 'bold', borderBottom: '2px solid black', paddingBottom: '15px' } },
 ]
 
+const totals = [
+  { label: 'rawIncome', title: 'Bénéfice brut', style: {} },
+  { label: 'incomeTax', title: 'Impôts sur les sociétés', style: {} },
+  { label: 'netIncome', title: 'Bénéfice net', style: { fontWeight: 'bold' } },
+  { label: 'dividend', title: 'Dividende versé', style: {} },
+  { label: 'netDividend', title: 'Dividende perçu', style: { fontWeight: 'bold' } },
+  { label: 'managerMonthlyIncome', title: 'Revenu mensuel moyen du dirigeant', style: { fontWeight: 'bold', fontSize: '1.2rem' } },
+]
 function ControlledField({ field, simulation, fieldIndex, simulationIndex, updateSimulation, checker }) {
   const [isError, setIsError] = useState(false)
 
@@ -73,6 +96,7 @@ function App() {
   const displayField = (field, simulation, fieldIndex, simulationIndex) => {
     switch (field.label) {
       case 'daysPerWeek':
+
         return (<ControlledField
           key={`${fieldIndex}-${simulationIndex}`}
           field={field}
@@ -82,17 +106,25 @@ function App() {
           updateSimulation={updateSimulation}
           checker={checkDaysPerWeek}
         />)
-        case 'yearlyIncome':
-        case 'monthlyIncome':
-          return (
-            <div
-              key={`${fieldIndex}-${simulationIndex}`}
-              style={{ ...field.style, gridColumn: simulationIndex + 2, gridRow: fieldIndex + 1 }}
-              className={field.label}
-            >
-              {simulation[field.label]}
-            </div>
-          )
+      case 'yearlyIncome':
+      case 'monthlyIncome':
+      case 'yearlyTotalCost':
+      case 'montlyAverageCost':
+      case 'rawIncome':
+      case 'incomeTax':
+      case 'netIncome':
+      case 'dividend':
+      case 'netDividend':
+      case 'managerMonthlyIncome':
+        return (
+          <div
+            key={`${fieldIndex}-${simulationIndex}`}
+            style={{ ...field.style, gridColumn: simulationIndex + 2, gridRow: fieldIndex + 1 }}
+            className={field.label}
+          >
+            {simulation[field.label]}
+          </div>
+        )
 
       default:
         console.log({ ...field.style, gridColumn: simulationIndex + 2, gridRow: fieldIndex + 1 })
@@ -125,14 +157,19 @@ function App() {
 
       <div className="wrapper" style={styles.wrapper}>
         {fields.map((field, index) => (
-          <div key={index} style={{...field.style, gridColumn: 1, gridRow: index + 1 }} className={field.label}>
+          <div key={index} style={{ ...field.style, gridColumn: 1, gridRow: index + 1 }} className={field.label}>
             {field.title}
           </div>
         ))}
         < h3>Charges</h3>
         {expenses.map((expense, index) => (
-          <div key={index} style={{ gridColumn: 1, gridRow: index + fields.length + 1 + 1 }} className={expense.label}>
+          <div key={index} style={{ ...expense.style, gridColumn: 1, gridRow: index + fields.length + 1 + 1 }} className={expense.label}>
             {expense.title}
+          </div>
+        ))}
+        {totals.map((total, index) => (
+          <div key={index} style={{ ...total.style, gridColumn: 1, gridRow: index + fields.length + expenses.length + 1 + 1 }} className={total.label}>
+            {total.title}
           </div>
         ))}
 
@@ -140,6 +177,18 @@ function App() {
         {simulations.map((simulation, simulationIndex) => (
           fields.map((field, fieldIndex) => {
             return displayField(field, simulation, fieldIndex, simulationIndex)
+          })
+        ))}
+
+        {simulations.map((simulation, simulationIndex) => (
+          expenses.map((expense, expenseIndex) => {
+            return displayField(expense, simulation, expenseIndex + fields.length + 1, simulationIndex)
+          })
+        ))}
+
+        {simulations.map((simulation, simulationIndex) => (
+          totals.map((total, totalIndex) => {
+            return displayField(total, simulation, totalIndex + fields.length + expenses.length + 1, simulationIndex)
           })
         ))}
       </div>
@@ -150,8 +199,8 @@ function App() {
 const styles = {
   wrapper: {
     display: "grid",
-    gridTemplateColumns: "repeat(3, 1fr)",
-    gridGap: "10px",
+    // gridTemplateColumns: "repeat(3, 1fr)",
+    gridGap: "2px",
     // gridAutoRows: "minmax(100px, auto)"
   }
 }
--- src/models/simulation.js
+++ src/models/simulation.js
@@ -1,4 +1,5 @@
 const WEEKS_IN_YEAR = 52;
+const TAX_THRESHOLD = 42500;
 
 export class Simulation {
   constructor(initialValues = {}) {
@@ -7,36 +8,104 @@ export class Simulation {
     this._weeksOff = initialValues.weeksOff || 10;
     this._weeksOn = initialValues.weeksOn || 42;
     this._daysPerWeek = initialValues.daysPerWeek || 5;
+    this._dailyFoodCost = initialValues.dailyFoodCost || 10;
+    this._yearlyRent = initialValues.yearlyRent || 3500;
+    this._yearlyAccountingCost = initialValues.yearlyAccountingCost || 1200;
+    this._yearlyInternetCost = initialValues.yearlyInternetCost || 0;
+    this._yearlyPhoneCost = initialValues.yearlyPhoneCost || 0;
+    this._yearlyProInsuranceCost = initialValues.yearlyProInsuranceCost || 450;
+    this._yearlyOtherInsuranceCost = initialValues.yearlyOtherInsuranceCost || 0;
+    this._yearlyBankingCost = initialValues.yearlyBankingCost || 120;
+    this._yearlyFurnitureCost = initialValues.yearlyFurnitureCost || 400;
+    this._yearlyOtherCost = initialValues.yearlyOtherCost || 100;
   }
+
 
   get weeksOff() {
     return this._weeksOff;
   }
-
   set weeksOff(value) {
     this._weeksOff = parseInt(value);
     this._weeksOn = WEEKS_IN_YEAR - this._weeksOff;
   }
-
   get weeksOn() {
     return this._weeksOn;
   }
-
   set weeksOn(value) {
     this._weeksOn = parseInt(value);
     this._weeksOff = WEEKS_IN_YEAR - this._weeksOn;
   }
-
   get daysPerWeek() {
     return this._daysPerWeek;
   }
-
   set daysPerWeek(value) {
     if (value < 0 || value > 7) {
       throw new Error('daysPerWeek must be between 0 and 7');
     }
     this._daysPerWeek = value;
   }
+  get dailyFoodCost() {
+    return this._dailyFoodCost;
+  }
+  set dailyFoodCost(value) {
+    return this._dailyFoodCost = parseFloat(value) || 0;
+  }
+  get yearlyRent() {
+    return this._yearlyRent
+  }
+  set yearlyRent(value) {
+    this._yearlyRent = parseFloat(value) || 0
+  }
+  get yearlyAccountingCost() {
+    return this._yearlyAccountingCost
+  }
+  set yearlyAccountingCost(value) {
+    this._yearlyAccountingCost = parseFloat(value) || 0
+  }
+  get yearlyInternetCost() {
+    return this._yearlyInternetCost
+  }
+  set yearlyInternetCost(value) {
+    this._yearlyInternetCost = parseFloat(value) || 0
+  }
+  get yearlyPhoneCost() {
+    return this._yearlyPhoneCost
+  }
+  set yearlyPhoneCost(value) {
+    this._yearlyPhoneCost = parseFloat(value) || 0
+  }
+  get yearlyProInsuranceCost() {
+    return this._yearlyProInsuranceCost
+  }
+  set yearlyProInsuranceCost(value) {
+    this._yearlyProInsuranceCost = parseFloat(value) || 0
+  }
+  get yearlyOtherInsuranceCost() {
+    return this._yearlyOtherInsuranceCost
+  }
+  set yearlyOtherInsuranceCost(value) {
+    this._yearlyOtherInsuranceCost = parseFloat(value) || 0
+  }
+  get yearlyBankingCost() {
+    return this._yearlyBankingCost
+  }
+  set yearlyBankingCost(value) {
+    this._yearlyBankingCost = parseFloat(value) || 0
+  }
+  get yearlyFurnitureCost() {
+    return this._yearlyFurnitureCost
+  }
+  set yearlyFurnitureCost(value) {
+    this._yearlyFurnitureCost = parseFloat(value) || 0
+  }
+  get yearlyOtherCost() {
+    return this._yearlyOtherCost
+  }
+  set yearlyOtherCost(value) {
+    this._yearlyOtherCost = parseFloat(value) || 0
+  }
+
+
 
   serialize() {
     return {
@@ -47,6 +116,25 @@ export class Simulation {
       daysPerWeek: this.daysPerWeek,
       yearlyIncome: this.yearlyIncome(),
       monthlyIncome: this.monthlyIncome(),
+      dailyFoodCost: this.dailyFoodCost,
+      yearlyFoodCost: this.yearlyFoodCost(),
+      yearlyRent: this.yearlyRent,
+      yearlyAccountingCost: this.yearlyAccountingCost,
+      yearlyInternetCost: this.yearlyInternetCost,
+      yearlyPhoneCost: this.yearlyPhoneCost,
+      yearlyProInsuranceCost: this.yearlyProInsuranceCost,
+      yearlyOtherInsuranceCost: this.yearlyOtherInsuranceCost,
+      yearlyBankingCost: this.yearlyBankingCost,
+      yearlyFurnitureCost: this.yearlyFurnitureCost,
+      yearlyOtherCost: this.yearlyOtherCost,
+      yearlyTotalCost: this.yearlyTotalCost(),
+      montlyAverageCost: this.montlyAverageCost(),
+      rawIncome: this.rawIncome(),
+      incomeTax: this.incomeTax(),
+      netIncome: this.netIncome(),
+      dividend: this.netIncome(),
+      netDividend: this.netDividend(),
+      managerMonthlyIncome: this.managerMonthlyIncome(),
     }
   }
 
@@ -58,6 +146,51 @@ export class Simulation {
     return this.yearlyIncome() / 12;
   }
 
+  yearlyFoodCost() {
+    return this.dailyFoodCost * this.daysPerWeek * this.weeksOn;
+  }
+
+  yearlyTotalCost() {
+    return this.yearlyFoodCost()
+      + this.yearlyRent
+      + this.yearlyAccountingCost
+      + this.yearlyInternetCost
+      + this.yearlyPhoneCost
+      + this.yearlyProInsuranceCost
+      + this.yearlyOtherInsuranceCost
+      + this.yearlyBankingCost
+      + this.yearlyFurnitureCost
+      + this.yearlyOtherCost;
+  }
+
+  montlyAverageCost() {
+    return this.yearlyTotalCost() / 12.0;
+  }
+
+  rawIncome() {
+    return this.yearlyIncome() - this.yearlyTotalCost();
+  }
+
+  incomeTax() {
+    if (this.rawIncome() <=0) {
+      return 0
+    }
+    const belowThreshold = Math.min(this.rawIncome(), TAX_THRESHOLD);
+    const overThreshold = Math.max(this.rawIncome() - TAX_THRESHOLD, 0);
+    return 0.15 * belowThreshold + 0.25 * overThreshold;
+  }
+
+  netIncome() {
+    return this.rawIncome() - this.incomeTax();
+  }
+
+  netDividend() {
+    return this.netIncome() * 0.7;
+  }
+
+  managerMonthlyIncome() {
+    return this.netDividend() / 12;
+  }
 }
 
 
