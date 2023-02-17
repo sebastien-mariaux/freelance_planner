1b236c246787f4099b6b5f6a4d8f1023c032db24
Sébastien <sebastien.mariaux@protonmail.com>
2023-02-17 19:37:44 +0100

    Add layout and routes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import Simulations from './Simulations/Simulations';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import About from './About';
+import SiteLayout from './Simulations/SiteLayout';
 
 function App() {
+
   return (
-    < div style={{ margin: '0 50px 20px 50px' }}>
-      <header style={{ display: 'flex' }} >
-        <h1 style={{ display: 'block' }}>FREELANCE PLANNER</h1>
-        <a
-        href='https://sebastien-mariaux.com/'
-        alt='A  propos du site'
-        style={{ marginLeft: 'auto', marginTop: 'auto', marginBottom: 'auto' }}
-        >
-          À propos...
-        </a>
-      </header>
-      <Simulations />
-      <footer style={{ marginTop: '50px' }} >
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <span>&copy;&nbsp;</span>
-          <a href='https://sebastien-mariaux.com/' alt='site du créateur'>
-            Sébastien Mariaux
-          </a>
-          <span>&nbsp;- 2023</span>
-        </div>
-      </footer>
-    </div>
+    <Router>
+
+
+      <Routes>
+        <Route element={<SiteLayout />}>
+          <Route path="/a-propos" element={<About />} />
+          <Route path="/" element={<Simulations />} />
+        </Route>
+      </Routes>
+
+    </Router>
   )
 }
 
-const styles = {
 
-}
 
 export default App;
---
+++ src/Simulations/SiteLayout.js
@@ -0,0 +1,28 @@
+import { Outlet } from "react-router-dom";
+
+export default function SiteLayout({children}){
+  return(
+    < div style={{ margin: '0 50px 20px 50px' }}>
+    <header style={{ display: 'flex' }} >
+      <h1 style={{ display: 'block' }}>FREELANCE PLANNER</h1>
+      <a
+        href='/a-propos'
+        alt='A  propos du site'
+        style={{ marginLeft: 'auto', marginTop: 'auto', marginBottom: 'auto' }}
+      >
+        À propos...
+      </a>
+    </header>
+    <Outlet/>
+    <footer style={{ marginTop: '50px' }} >
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <span>&copy;&nbsp;</span>
+        <a href='https://sebastien-mariaux.com/' alt='site du créateur'>
+          Sébastien Mariaux
+        </a>
+        <span>&nbsp;- 2023</span>
+      </div>
+    </footer>
+  </div>
+  )
+}
