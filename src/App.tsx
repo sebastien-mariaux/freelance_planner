a53e40fae4514ad02dbd08e2c8f77632f1362874
Sébastien Mariaux <sebastien.mariaux@protonmail.com>
2023-03-18 13:19:59 +0100

    Handle errors

--- src/App.tsx
+++ src/App.tsx
@@ -11,21 +11,31 @@ import About from './About';
 import SiteLayout from './Simulations/SiteLayout';
 import Expenses from './Expenses/Expenses';
 import Achieved from './Achieved/Achieved';
+import { ErrorBoundary } from 'react-error-boundary';
+import MainFallbackError from './errors/MainFallbackError';
 
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route element={<SiteLayout />}>
-          <Route path="/a-propos" element={<About />} />
-          <Route path="/" element={<Simulations />} />
-          <Route path="/simulations" element={<Simulations />} />
-          <Route path="/expenses" element={<Expenses />} />
-          <Route path="/achieved" element={<Achieved />} />
-        </Route>
-      </Routes>
-    </Router>
+    <ErrorBoundary
+      FallbackComponent={MainFallbackError}
+      onError={(error, componentStack) => {
+        console.log('error', error)
+        console.log('componentStack', componentStack)
+      }}
+    >
+      <Router>
+        <Routes>
+          <Route element={<SiteLayout />}>
+            <Route path="/a-propos" element={<About />} />
+            <Route path="/" element={<Simulations />} />
+            <Route path="/simulations" element={<Simulations />} />
+            <Route path="/expenses" element={<Expenses />} />
+            <Route path="/achieved" element={<Achieved />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ErrorBoundary>
   )
 }
 
--- src/Simulations/simulationsHelper.ts
+++ src/Simulations/simulationsHelper.ts
@@ -1,7 +1,11 @@
 export const displayAmount = (amount: number) => {
+  if (amount === undefined || amount === null) return '???';
+
   return amount.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' });
 }
 
 export const displayPercent = (amount: number) => {
+  if (amount === undefined || amount === null) return '???';
+
   return amount.toLocaleString('fr-FR', { style: 'percent', minimumFractionDigits: 2 });
 }
